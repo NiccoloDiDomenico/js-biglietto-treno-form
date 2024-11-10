03bbfc09d5fb8f822549b0c61b91d6b9cba18a1b
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-11-11 00:29:29 +0100

    feat: add Milestone2

--- js/script.js
+++ js/script.js
@@ -5,24 +5,34 @@ let discount;
 
 // INPUT
 const ticketFormInput = document.getElementById("ticket-form")
+const userNameInput = document.getElementById("user-name")
 const userKmInput = document.getElementById("user-km")
 const userAgeInput = document.getElementById("user-age")
 const generatesBtnInput = document.getElementById("generates-btn")
 
-console.log(ticketFormInput, userKmInput, userAgeInput, generatesBtnInput);
+console.log(ticketFormInput, userNameInput, userKmInput, userAgeInput, generatesBtnInput);
+
+// ELEMENTS
+const ticketSectionElement = document.getElementById("ticket-section")
+const card1Element = document.getElementById("card1")
+const card2Element = document.getElementById("card2")
+const card3Element = document.getElementById("card3")
+const card4Element = document.getElementById("card4")
+console.log(ticketSectionElement, card1Element, card2Element, card3Element, card4Element);
 
 
-// FORM
+// CALCULATOR SECTION
 ticketFormInput.addEventListener("submit", function (event) {
     event.preventDefault();
     console.log(event);
-    
+
     // valori del form
+    const userNameValue = userNameInput.value.trim();
     const userKmValue = parseInt(userKmInput.value.trim());
     const userAgeValue = userAgeInput.value.trim();
-    console.log(userKmValue, userAgeValue);
-    console.log(typeof userKmValue, typeof userAgeValue);
-    
+    console.log(userNameValue, userKmValue, userAgeValue);
+    console.log(typeof userNameValue, typeof userKmValue, typeof userAgeValue);
+
     // calcolo il prezzo intero in base ai km dell'utente
     let fullPrice = (userKmValue * priceForKm);
     console.log(fullPrice);
@@ -36,13 +46,26 @@ ticketFormInput.addEventListener("submit", function (event) {
     } else {
         discount = 0;
     }
-    
+
     // calcolo il prezzo finale del biglietto
     const finalPrice = (fullPrice - discount);
     console.log(finalPrice);
-    
 
-    ticketFormInput.reset();
+    // TICKET SECTION
+    generatesBtnInput.addEventListener("click", function () {
+        ticketSectionElement.classList.remove("d-none");
+
+        card1Element.innerHTML = userNameValue;
+        card2Element.innerHTML = fullPrice.toFixed(2) + " €";
+        if (userAgeValue === `ragazzo`) {
+            card3Element.innerHTML = `20% di sconto`;
+        } else if (userAgeValue === `anziano`) {
+            card3Element.innerHTML = `40% di sconto`;
+        } else {
+            card3Element.innerHTML = `Nessuno sconto`;
+        }
+        card4Element.innerHTML = finalPrice.toFixed(2) + " €";
+
+    })
 })
 
-    
